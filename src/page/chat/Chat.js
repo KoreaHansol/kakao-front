22511783729ab1570ebed62daceda830aecb7503
KoreaHansol <hskimaa@hanmail.net>
2022-02-09 22:07:32 +0900

    채팅방 css적용중

--- src/page/chat/Chat.js
+++ src/page/chat/Chat.js
@@ -1,13 +1,17 @@
-import React, { useCallback, useContext, useEffect } from 'react'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
 import { useNavigate, useLocation } from 'react-router'
 import 'page/room/Room'
 import socketIOClient from "socket.io-client"
 import _ from 'lodash'
 import req2svr from './req2svr'
 import './Chat.scss'
+import CustomInput from 'components/customInput/CustomInput'
+import CustomButton from 'components/customButton/CustomButton'
 const socket = socketIOClient( 'http://localhost:8080' )
 
 const Chat = () => {
+  const [ chatValue, setChatValue ] = useState( '' )
+
   let { state } = useLocation()
   const history = useNavigate()
   useEffect( () => {
@@ -20,6 +24,15 @@ const Chat = () => {
     }
 
   }, [] )
+
+  const inputChangeHandler = useCallback( ( e ) => {
+    setChatValue( e.target.value )
+  }, [] )
+
+  const sendClickHandler = () => {
+    console.log( 'send' )
+  }
+
   return (
     <div className="chat-room-wrapper">
       <div className='header'>
@@ -31,8 +44,9 @@ const Chat = () => {
           <div className='image'>
             <span className='person material-icons'>person</span>
           </div>
-          <div className='chat-box'>
-          sadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasd
+          <div className='content-box'>
+            <div className='name'>김한솔</div>
+            <div className='chat-box'>ㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇㅎㅇ</div>
           </div>
           <div className='time'>오전 9:30</div>
         </div>
@@ -40,14 +54,20 @@ const Chat = () => {
         <div className='my-chat'>
           <div className='time'>오전 9:30</div>
           <div className='chat-box'>
-          sadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasdsadasdasdasd
+            asdjiasodjoi
           </div>
         </div>
 
 
       </div>
       <div className='input-area'>
-
+        <textarea className="text-area" value={ chatValue } onChange={ inputChangeHandler }/>
+        <div className='btn-area'>
+        {/* onClick, children, disabled */}
+          <CustomButton onClick={ sendClickHandler } disabled={ !chatValue }>
+            <div>전송</div>
+          </CustomButton>
+        </div>
       </div>
     </div>
   )
