bb70f70ad553c264b95cf6e23e6661d39b1dac1d
KoreaHansol <hskimaa@hanmail.net>
2022-02-15 08:02:16 +0900

    황천의 토스트매니저 추가

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import 'App.scss'
-// import { useEffect, useContext } from 'react'
+import { useEffect, useContext } from 'react'
 import Routes from 'router/Routes'
 import toastManager from 'toastManager'
-// import eventBus from 'eventBus'
-// import { Context } from "context/context"
+import eventBus from 'eventBus'
+import { Context } from "context/context"
 
 const App = () => {
-  // const { user, roomId } = useContext( Context )
+  const { user, roomId } = useContext( Context )
 
-  // useEffect( () => {
-  //   eventBus.on( 'fromChat', data => {
-  //     console.log( roomId )
-  //     if( data.fromUserId != user.userId && roomId !== data.refreshRoomId ) {
-  //       toastManager.open( { message: data.message, userName: data.fromUserName } )
-  //     }
-  //   } )
-  // }, [ roomId ] )
+  useEffect( () => {
+    eventBus.on( 'fromChat', data => {
+        toastManager.open( { message: data.message, userName: data.fromUserName } )
+    } )
+  }, [] )
   return (
     <>
       <div className="app">
         <Routes/>
       </div>
       <div className='toast-anchor'>
-        {/* { toastManager.Render() } */}
+        { toastManager.Render() }
       </div>
     </>
   )
